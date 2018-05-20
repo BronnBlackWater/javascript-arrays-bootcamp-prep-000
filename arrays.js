586876a0d66364b1998847289135efeb95185289
Bronn BlackWater <bblackwater9@gmail.com>
2018-05-20 03:08:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  newArray = [element,...array]
-  array = newArray
+  array.unshift(element)
   return array
 }
