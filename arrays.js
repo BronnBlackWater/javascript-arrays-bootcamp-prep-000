8c271654f70cd15d65cae638b7bfe627474a6554
Bronn BlackWater <bblackwater9@gmail.com>
2018-05-20 02:36:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray(array, element) {
   return [element,...array]
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push(element)
+}
